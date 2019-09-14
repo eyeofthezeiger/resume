e547eb3c6b84f60037b0123b4a7856bbdfe0a906
Matt Zeiger <matthew.zeiger47@gmail.com>
2019-09-14 14:06:47 -0400

    Committing intial portfolio files

---
+++ src/components/about/about.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+export default class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <section id="about">
+            <div className="row">
+            <div className="three columns">
+                <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+            </div>
+            <div className="nine columns main-col">
+                <h2>About Me</h2>
+                <p>Hi there, thank you for taking the time to visit my website. 
+                  My name is Matthew Zeiger and i'm from Warrington, PA a little suburb of philadelphia about an hour from the city.
+                  I studied at Penn State University from 2012 to 2016 where I learned Information Sciences and Technology with a focus on People, Society and Organizations. 
+                  I know that's a mouthful but it deals with human computer interaction. 
+                  After college I spent most of my time at Liberty Mutual as a software developer on a User Experience team, and recently I chose to try out a new career as a recruiting and technology associate in an early stage startup called Percy.
+                  It has been a wild ride there and I have come to the conclusion that I have a passion for Software Development and am looking forward to the next step in mhy career.
+                </p>
+                <div className="row">
+                <div className="columns contact-details">
+                    <h2>Contact Details</h2>
+                    <p className="address">
+                    <span>Matthew Zeiger</span><br />
+                    <span>Boston, MA<br />
+                    </span>
+                    <span>matthew.zeiger2296@gmail.com</span>
+                    </p>
+                </div>
+                <div className="columns download">
+                    <p>
+                    <a href="#" className="button"><i className="fa fa-download" />Download Resume</a>
+                    </p>
+                </div>
+                </div> {/* end row */}
+            </div> {/* end .main-col */}
+            </div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header id="home">
+            <nav id="nav-wrap">
+                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <ul id="nav" className="nav">
+                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                <li><a className="smoothscroll" href="#about">About</a></li>
+                <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            </ul> {/* end #nav */}
+            </nav> {/* end #nav-wrap */}
+            <div className="row banner">
+            <div className="banner-text">
+                <h1 className="responsive-headline">Matthew Zeiger</h1>
+                <h3>I'm a <span>software developer</span>, <span>recruiter</span> and <span>aspiring writer</span> honing my craft and searching for a new oppurtunity as a developer. Let's <a className="smoothscroll" href="#about">start scrolling </a>
+               and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                <hr />
+                <ul className="social">
+                <li><a href="https://www.facebook.com/matt.zeiger.10?ref=bookmarks" target="_blank"><i className="fa fa-facebook" /></a></li>
+                <li><a href="https://www.linkedin.com/in/matthewzeiger/" target="_blank"><i className="fa fa-linkedin" /></a></li>
+                <li><a href="https://github.com/eyeofthezeiger" target="_blank"><i className="fa fa-github" /></a></li>
+                </ul>
+            </div>
+            </div>
+            <p className="scrolldown">
+            <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
+            </p>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
